51c8b4007964d7b2da0e00c909132603451e35ac
Daniel Haiem <dnhaiem@gmail.com>
2022-06-20 17:02:47 +0300

    milestone 2 Edit index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,27 @@
-let num = 3;
-let result = 2;
-let fibText = `The Fibonacci of ${num} is ${result}`;
-
 const fibTextEl = document.getElementById("fib-text-el");
 
-function getFibText() {
+let fib = [0, 1];
+
+function getFibText(num) {
+  for (let i = fib.length; i < num; i++) {
+    fib[i] = fib[i - 2] + fib[i - 1];
+  }
+
+  let last2 = fib.slice(-2);
+
+  let result = "";
+
+  if (num < 2) {
+    result = 0;
+  } else {
+    result = last2.reduce(
+      (previousValue, currentValue) => previousValue + currentValue,
+      0
+    );
+  }
+
+  let fibText = `The Fibonacci of ${num} is ${result}`;
   fibTextEl.innerText = fibText;
 }
 
-getFibText();
+getFibText(19);
